c39500f1bcb9b76f7939a8966f9be18f79ae8d17
Le_PC_qui_parle <nicolas.cleuvenotbilariki@gmail.com>
2024-08-20 17:46:36 +0200

    Update script.js

--- tutos/1/variables/script.js
+++ tutos/1/variables/script.js
@@ -4,6 +4,8 @@ function validerVar() {
         alert("Bravo !")
     } else if (reponse === 'nom = "Tommy"') {
         alert("Bravo !")
+    } else if (reponse === "nom = Tommy") {
+        alert("Attention aux guillemets !")
     } else {
         alert("Faux ! Relisez bien la syntaxe et faites attention aux espaces et majuscules")
     }
