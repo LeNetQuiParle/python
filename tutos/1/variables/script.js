c99a971ba6c20d641efabcdbe7bfe298c2efd324
Le_PC_qui_parle <nicolas.cleuvenotbilariki@gmail.com>
2024-08-20 17:37:27 +0200

    create script.js and update index.html

---
+++ tutos/1/variables/script.js
@@ -0,0 +1,8 @@
+function validerVar() {
+    let reponse = document.getElementById("reponse");
+    if (reponse === "nom = Tommy") {
+        alert("Bravo !")
+    } else {
+        alert("Faux ! Relisez bien la syntaxe et faites attention aux espaces et majuscules")
+    }
+}
